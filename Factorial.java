0b4e32c504096b40929e71536c4a5ea211d170a3
Drishti0348 <71304344+Drishti0348@users.noreply.github.com>
2020-09-15 18:52:47 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+
+public class Factorial {
+	public static void main(String args[])
+	{
+		int factorial=1;
+		for(int i=2;i<=6;i++)
+			factorial=factorial*i;
+		System.out.println(factorial);
+		
+		String[] name = new String[] {"A","B","C","D","E","F","G","H","I","J"};
+		for(int i=0;i<10;i++)
+		System.out.println("Hey "+name[i]+"!");
+	}
+
+}
